d531de73c64c6942403d0db9140a2a3646c35f0b
sondremann2015 <68125004+sondremann2015@users.noreply.github.com>
2021-05-01 16:46:20 +0200

    Update and rename __resource.lua to fxmanifest.lua

---
+++ speedometer/fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Manifest Version
+fx_version 'bodacious'
+game 'gta5'
+-- UI
+ui_page "ui/index.html"
+files {
+	"ui/*.html",
+	"ui/*.css",
+	"ui/*.js",
+	"ui/*.svg",
+	"ui/*.ttf",
+}
+
+-- Client Scripts
+client_scripts {
+	"client.lua",
+}
